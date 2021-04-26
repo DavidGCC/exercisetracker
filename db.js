9169986435248b32940cddbda8a3ea8cdeeb14f9
DavitG <datog309@gmail.com>
2021-04-26 13:52:03 +0400

    added remote db

--- db.js
+++ db.js
@@ -1,14 +1,14 @@
 const Pool = require("pg").Pool;
 
 
-const poolConf = {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME
-};
+// const poolConf = {
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASS,
+//     port: process.env.DB_PORT,
+//     database: process.env.DB_NAME
+// };
 
-const pool = new Pool(poolConf);
+const pool = new Pool({ connectionString: process.env.DB_REMOTE });
 
 module.exports = pool;
--- utility.js
+++ utility.js
@@ -1,14 +1,17 @@
 const formUser = (dbRows) => {
-    const modifiedRows = dbRows.reduce((rows, row) => {
-        const { description, duration, date } = row;
-        return {...rows, log: [...rows.log, { description, duration, date }] };
-    }, {
-        '_id': dbRows[0].userid,
-        username: dbRows[0].username,
-        log: []
-    });
-    modifiedRows.count = modifiedRows.log.length;
-    return modifiedRows;
+    if (dbRows) {
+        const modifiedRows = dbRows.reduce((rows, row) => {
+            const { description, duration, date } = row;
+            return {...rows, log: [...rows.log, { description, duration, date }] };
+        }, {
+            '_id': dbRows[0].userid,
+            username: dbRows[0].username,
+            log: []
+        });
+        modifiedRows.count = modifiedRows.log.length;
+        return modifiedRows;
+    }
+    return null;
 }
 
 module.exports = { formUser };
